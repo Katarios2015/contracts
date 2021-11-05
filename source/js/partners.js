8d3471e7e5719f2c5713f4a9a511ba36849a8f76
Katarios2015 <prichal7@bk.ru>
2021-11-05 11:44:31 +0300

    завершение верстки окна Создать

---
+++ source/js/partners.js
@@ -0,0 +1,7 @@
+var selectPartner = document.querySelector('.select__item--create');
+var partnerlist = document.querySelector('.partner-list');
+
+/*selectPartner.addEventListener("change", function() {
+
+}
+)*/
